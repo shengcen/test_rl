03388c65694a7df0d63a31a74babe424712feca0
shengcen <51124420+shengcen@users.noreply.github.com>
2023-10-16 21:14:39 +0800

    Update mpnn.py

--- mpnn.py
+++ mpnn.py
@@ -11,50 +11,6 @@ from torch_geometric.nn import GCNConv, global_mean_pool
 num_node_features = 10*6
 hidden = 64
 
-class GCN(torch.nn.Module):
-    def __init__(self, num_node_features):
-        super(GCN, self).__init__()
-        self.conv1 = GCNConv(num_node_features, hidden)
-        self.conv2 = GCNConv(hidden, hidden)
-        self.conv3 = GCNConv(hidden, hidden)
-        # self.lin = Linear(hidden, 1)
-        self.lin = Linear(hidden, 5)
-
-        self.lin1 = Linear(5+2, 5)
-        self.lin2 = Linear(5,3)
-
-    # def forward(self,x, edge_index, batch, dist, sw):
-    #     x= self.conv1(x, edge_index)
-    #     x= x.relu()
-    #     x = self.conv2(x, edge_index)
-    #     x = x.relu()
-    #     x = self.conv3(x, edge_index)
-    #     x = global_mean_pool(x, batch)
-    #     x = F.dropout(x, p=0.5,training=self.training)
-    #     x = self.lin(x)
-    #     # print(x.shape)
-    #     # print(sw.shape)
-    #     x = self.lin1(torch.cat((x,dist,sw), dim=1))
-    #     x = x.relu()
-    #     x = self.lin2(x)
-    #     return x
-
-    def forward(self, x, edge_index, batch):
-        x = self.conv1(x, edge_index)
-        x = x.relu()
-        x = self.conv2(x, edge_index)
-        x = x.relu()
-        x = self.conv3(x, edge_index)
-        x = global_mean_pool(x, batch)
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = self.lin(x)
-        # print(x.shape)
-        # print(sw.shape)
-        # x = self.lin1(torch.cat((x, dist, sw), dim=1))
-        x = x.relu()
-        x = self.lin2(x)
-        return x
-
 class GCN_test(torch.nn.Module):
     def __init__(self):
         super(GCN_test, self).__init__()
@@ -93,77 +49,10 @@ class GCN_test(torch.nn.Module):
 
 
 
-        x = torch.unsqueeze(x,0)
+        # x = torch.unsqueeze(x,0)
         return x
 
 
-import tensorflow as tf
-from tensorflow import keras
-from keras import regularizers
-
-class myModel(tf.keras.Model):
-    def __init__(self, hparams):
-        super(myModel, self).__init__()
-        self.hparams = hparams
-
-        # Define layers here
-        self.Message = tf.keras.models.Sequential()
-        self.Message.add(keras.layers.Dense(self.hparams['link_state_dim'],
-                                            activation=tf.nn.selu, name="FirstLayer"))
-
-        self.Update = tf.keras.layers.GRUCell(self.hparams['link_state_dim'], dtype=tf.float32)
-
-        self.Readout = tf.keras.models.Sequential()
-        self.Readout.add(keras.layers.Dense(self.hparams['readout_units'],
-                                            activation=tf.nn.selu,
-                                            kernel_regularizer=regularizers.l2(hparams['l2']),
-                                            name="Readout1"))
-        self.Readout.add(keras.layers.Dropout(rate=hparams['dropout_rate']))
-        self.Readout.add(keras.layers.Dense(self.hparams['readout_units'],
-                                            activation=tf.nn.selu,
-                                            kernel_regularizer=regularizers.l2(hparams['l2']),
-                                            name="Readout2"))
-        self.Readout.add(keras.layers.Dropout(rate=hparams['dropout_rate']))
-        self.Readout.add(keras.layers.Dense(1, kernel_regularizer=regularizers.l2(hparams['l2']),
-                                            name="Readout3"))
-
-    def build(self, input_shape=None):
-        self.Message.build(input_shape=tf.TensorShape([None, self.hparams['link_state_dim']*2]))
-        self.Update.build(input_shape=tf.TensorShape([None,self.hparams['link_state_dim']]))
-        self.Readout.build(input_shape=[None, self.hparams['link_state_dim']])
-        self.built = True
-
-    @tf.function
-    def call(self, states_action, states_graph_ids, states_first, states_second, sates_num_edges, training=False):
-        # Define the forward pass
-        link_state = states_action
-
-        # Execute T times
-        for _ in range(self.hparams['T']):
-            # We have the combination of the hidden states of the main edges with the neighbours
-            mainEdges = tf.gather(link_state, states_first)
-            neighEdges = tf.gather(link_state, states_second)
-
-            edgesConcat = tf.concat([mainEdges, neighEdges], axis=1)
-
-            ### 1.a Message passing for link with all it's neighbours
-            outputs = self.Message(edgesConcat)
-
-            ### 1.b Sum of output values according to link id index
-            edges_inputs = tf.math.unsorted_segment_sum(data=outputs, segment_ids=states_second,
-                                                        num_segments=sates_num_edges)
-
-            ### 2. Update for each link
-            # GRUcell needs a 3D tensor as state because there is a matmul: Wrap the link state
-            outputs, links_state_list = self.Update(edges_inputs, [link_state])
-
-            link_state = links_state_list[0]
-
-        # Perform sum of all hidden states
-        edges_combi_outputs = tf.math.segment_sum(link_state, states_graph_ids, name=None)
-
-        r = self.Readout(edges_combi_outputs,training=training)
-        return r
 
 
 
